e1cf63a660ba9461e91e0391f0f028b40d7586be
Sujoy Giri <girisujoy630@gmail.com>
2024-01-09 20:59:31 +0530

    lets start

---
+++ script.js
@@ -0,0 +1,25 @@
+
+(function (){
+    let arraySize = null;
+    const arrayInitializeBtnNode = document.getElementById("initialize-btn")
+    const arrayContainerNode = document.querySelector(".array-container")
+
+    function renderEmptyArray(size){
+        const elementHtml = `<div class="element"></div>`
+        arrayContainerNode.innerHTML = '';
+        for(let index = 0; index < size; ++index){
+            arrayContainerNode.innerHTML += elementHtml
+        }
+    }
+
+    arrayInitializeBtnNode.addEventListener("click",()=>{
+        arraySize = Number.parseInt(window.prompt("Enter array size :- "))
+        console.log(arraySize);
+        if(arraySize && arraySize > 0 && arraySize <= 10){
+            renderEmptyArray(arraySize)
+        }else{
+            window.alert("Please enter array size in between 1 to 20")
+        }
+       
+    })
+})()

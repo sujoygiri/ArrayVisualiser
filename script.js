b0261343285dfdf6e2f72bb24de81ee57bd28dc0
Sujoy Giri <girisujoy630@gmail.com>
2024-02-07 12:19:44 +0530

    ongoing

--- script.js
+++ script.js
@@ -4,23 +4,26 @@
     const arrayInitializeBtnNode = document.getElementById("initialize-btn")
     const arrayContainerNode = document.querySelector(".array-container")
 
-    function renderEmptyArray(size){
-        const elementHtml = `<div class="element"></div>`
-        arrayContainerNode.innerHTML = '';
-        for(let index = 0; index < size; ++index){
-            arrayContainerNode.innerHTML += elementHtml
-        }
-    }
+    // function renderEmptyArray(size){
+    //     const elementHtml = `<div class="element"></div>`
+    //     arrayContainerNode.innerHTML = '';
+    //     for(let index = 0; index < size; ++index){
+    //         arrayContainerNode.innerHTML += elementHtml
+    //     }
+    // }
 
-    arrayInitializeBtnNode.addEventListener("click",()=>{
-        arraySize = Number.parseInt(window.prompt("Enter array size :- "))
-        console.log(arraySize);
-        if(arraySize && arraySize > 0 && arraySize <= 10){
-            renderEmptyArray(arraySize)
-            arrayInitializeBtnNode.style.display = "none"
-        }else{
-            window.alert("Please enter array size in between 1 to 10")
-        }
+    // arrayInitializeBtnNode.addEventListener("click",()=>{
+    //     arraySize = Number.parseInt(window.prompt("Enter array size :- "))
+    //     console.log(arraySize);
+    //     if(arraySize && arraySize > 0 && arraySize <= 10){
+    //         // renderEmptyArray(arraySize)
+    //         arrayInitializeBtnNode.style.display = "none"
+    //     }else{
+    //         window.alert("Please enter array size in between 1 to 10")
+    //     }
        
-    })
+    // })
+
+    arrayContainerNode.childNodes[0] = `<p>12</p>`
+    console.log(arrayContainerNode.replaceChildren());
 })()
